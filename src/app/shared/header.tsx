efd4b64dbeb83897ece779fe0a0e1eaa6edcf07a
Bigyan Chapagain <bigyanchapagain@gmail.com>
2025-01-05 17:58:46 -0600

    In navigation header, the active page is shown active by yellow underline.

--- src/app/shared/header.tsx
+++ src/app/shared/header.tsx
@@ -1,27 +1,56 @@
 "use client"
 
 import React, { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
 import SearchInput from "./../components/searchInput";
 
 const Header: React.FC = () => {
   const [searchValue, setSearchValue] = useState("");
+  const router = useRouter();
+  const pathname = usePathname();
+
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
   };
+
+  const isActive = (path: string) => pathname === path;
 
   return (
     <header className="bg-[#150B0B] text-white font-sans">
         <nav className="flex items-center justify-between px-2 py-4">
             <div className="flex items-center space-x-4">
               <div className="flex-shrink-0 w-16 pl-2">
-                <Image src="/images/logo.png" alt="Logo" width={40} height={40} />
+                <a href="/home">
+                  <Image src="/images/logo.png" alt="Logo" width={40} height={40} />
+                </a>
               </div>
               <div className="px-2">
                 <ul className="flex space-x-6">
-                  <li><a href="/home">HOME</a></li>
-                  <li><a href="/live">LIVE</a></li>
-                  <li><a href="/playlists">PLAYLISTS</a></li>
+                  <li>
+                    <a href="/home" className=
+                      {`${
+                        isActive("/home") ? "underline underline-offset-4 decoration-4 decoration-yellow-500" : ""
+                      }`}>
+                      HOME
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/live" className=
+                      {`${
+                        isActive("/live") ? "underline underline-offset-4 decoration-4 decoration-yellow-500" : ""
+                      }`}>
+                      LIVE
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/playlists" className=
+                      {`${
+                        isActive("/playlists") ? "underline underline-offset-4 decoration-4 decoration-yellow-500" : ""
+                      }`}>
+                      PLAYLISTS
+                    </a>
+                  </li>
                 </ul>
               </div>
             </div>
