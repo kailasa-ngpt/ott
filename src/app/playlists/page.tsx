5037a5c4101f70659a73f13cab7fcce08a99770d
Bigyan Chapagain <bigyanchapagain@gmail.com>
2025-01-11 15:37:48 -0600

    Playlists created statically. Todo next is populate this dynamically.

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,3 +1,4 @@
+import VideoSlider from "../components/videoSlider";
 import Footer from "../shared/footer";
 import Header from "../shared/header";
 
@@ -5,10 +6,24 @@ const Home = () => {
   return (
     <div className="bg-[#220E0E] text-white font-sans">
       <Header />
-      <main className="px-6 py-8">
-        <h1 className="text-4xl font-bold">Welcome to the PlayLists Page!</h1>
-      </main>
-      <Footer /> 
+      <div className="container mx-auto px-4 flex-grow min-h-screen">
+        <div className="mt-4">
+          <div className="w-full flex flex-col items-start">
+            <h2 className="text-xl font-bold mt-1">Playlist1</h2>
+            <hr className="w-full border-gray-500 my-2" />
+          </div>
+          <VideoSlider category="playlist1"  />
+        </div>
+
+        <div className="mt-4">
+          <div className="w-full flex flex-col items-start">
+            <h2 className="text-xl font-bold mt-1">Playlist2</h2>
+            <hr className="w-full border-gray-500 my-2" />
+          </div>
+          <VideoSlider category="playlist2"  />
+        </div>
+      </div>
+      <Footer/> 
     </div>
   );
 }
