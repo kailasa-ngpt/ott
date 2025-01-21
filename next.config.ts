5168713e9dae54e94e0dfbc478708355054b1221
Bigyan Chapagain <bigyanchapagain@gmail.com>
2025-01-20 21:22:22 -0600

    Commented module.exports as it is not needed in remote deploy but is required in local

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,13 @@ const nextConfig: NextConfig = {
   },
 };
 
-module.exports = {
-  reactStrictMode: false,
-  images: {
-    domains: ['pub-67940a2fbfa149ebb363693bbb5df7f0.r2.dev'], // Add your image domain here
-  },
-};
+// PLEASE DON'T DELETE FOLLOWING AS WE NEED THIS IN LOCAL DEPLOYMENT:
+
+// module.exports = {
+//   reactStrictMode: false,
+//   images: {
+//     domains: ['pub-67940a2fbfa149ebb363693bbb5df7f0.r2.dev'], // Add your image domain here
+//   },
+// };
 
 export default nextConfig;
