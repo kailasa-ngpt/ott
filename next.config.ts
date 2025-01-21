0199f90fcc61996aab7dcb4087dcf43dc48ba4a5
Bigyan Chapagain <bigyanchapagain@gmail.com>
2025-01-20 21:30:54 -0600

    Uncomment.

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,11 @@ const nextConfig: NextConfig = {
 
 // PLEASE DON'T DELETE FOLLOWING AS WE NEED THIS IN LOCAL DEPLOYMENT:
 
-// module.exports = {
-//   reactStrictMode: false,
-//   images: {
-//     domains: ['pub-67940a2fbfa149ebb363693bbb5df7f0.r2.dev'], // Add your image domain here
-//   },
-// };
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    domains: ['pub-67940a2fbfa149ebb363693bbb5df7f0.r2.dev'], // Add your image domain here
+  },
+};
 
 export default nextConfig;
