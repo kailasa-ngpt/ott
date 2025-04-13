e0cd7e91ae2e37cc488b23e646829ef5c8b448fa
kkailasa <kenyakailasa@gmail.com>
2025-04-13 22:00:49 +0300

    updated footer

--- Frontend/src/app/shared/footer.tsx
+++ Frontend/src/app/shared/footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC = () => {
     return (
       <footer className="py-4 bg-white text-center text-gray-600 text-sm w-full border-t">
         <p>
-          © 2024 Sri Nithyananda Paramashivam. All rights reserved. KAILASA's Nithyananda TV.
+          © 2025 Sri Nithyananda Paramashivam. All rights reserved. KAILASA's Nithyananda TV.
         </p>
         <div className="flex justify-center mt-2 space-x-4">
           {['facebook', 'twitter', 'youtube', 'instagram', 'website'].map((platform, index) => (
